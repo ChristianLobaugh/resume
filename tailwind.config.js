5dc939e789b79e68805464c3d791809517089961
Lobaugh, Christian <christian.lobaugh@hireright.com>
2020-02-17 03:21:40 -0600

    add lato font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   theme: {
     extend: {
       minHeight: {
         '3/4': '75%',
+      },
+      fontFamily: {
+        sans: ["Lato", ...defaultTheme.fontFamily.sans]
       }
     },
   },
